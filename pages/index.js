e70163360f70a289dd87bb613598520f59b74e0c
hubukala <hubertbukala@icloud.com>
2022-11-08 13:04:18 +0100

    hero component added

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import Header from "../components/header/header"
+import Hero from "../components/main/Hero"
 
 export default function Home() {
   return (
-    <header>
-      <Header />
-    </header>
+    <div className="App">
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Hero />
+      </main>
+    </div>
   )
 }
