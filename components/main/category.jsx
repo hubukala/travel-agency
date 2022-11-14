972a97010333361d1c17f8c97c850d5cb09745a7
hubukala <hubertbukala@icloud.com>
2022-11-14 18:05:38 +0100

    added component for category section

---
+++ components/main/category.jsx
@@ -0,0 +1,14 @@
+import styles from '../../styles/main/category.module.scss'
+
+const Category = () => {
+    return (
+        <section className={styles.sectionCategory}>
+            <div className={styles.categoryDescription}>
+                <h3>CATEGORY</h3>
+                <h1>We Offer Best Services</h1>
+            </div>
+        </section>
+    )
+}
+
+export default Category;
