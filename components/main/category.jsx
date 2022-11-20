212f4cc701cc653eee2ec2c206942e68d6139b68
hubukala <hubertbukala@icloud.com>
2022-11-20 13:24:22 +0100

    added image component to tile

--- components/main/category.jsx
+++ components/main/category.jsx
@@ -1,3 +1,4 @@
+import CategoryTile from './CategoryTile';
 import styles from '../../styles/main/category.module.scss'
 
 const Category = () => {
@@ -6,6 +7,28 @@ const Category = () => {
             <div className={styles.categoryDescription}>
                 <h3>CATEGORY</h3>
                 <h1>We Offer Best Services</h1>
+            </div>
+            <div className={styles.tilesContainer}>
+                <CategoryTile
+                    image = "/weather.png"
+                    title = "Calculated Weather"
+                    description = "Built Wicket longer admire do barton vanity itself do in it."
+                />
+                <CategoryTile
+                    image = "/plane1.png"
+                    title = "Best Flights"
+                    description = "Engrossed listening. Park gate sell they west hard for the."
+                />
+                <CategoryTile
+                    image = "/event.png"
+                    title = "Local Events"
+                    description = "Barton vanity itself do in it. Preferd to men it engrossed listening. "
+                />
+                <CategoryTile
+                    image = "/custom.png"
+                    title = "Customization"
+                    description = "We deliver outsourced aviation services for military customers"
+                />
             </div>
         </section>
     )
